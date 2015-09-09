81476290ed1343a2f5ebc4cfba4a2cc08d8ce589
Partha Aji <paji@redhat.com>
2015-09-09 16:32:21 -0400

    Fixes #10052 - Code to setup rhsm.conf for atomic hosts

--- manifests/katello.pp
+++ manifests/katello.pp
@@ -13,10 +13,18 @@ class certs::katello (
   }
 
   $katello_www_pub_dir            = '/var/www/html/pub'
+
   $rhsm_ca_dir                    = '/etc/rhsm/ca'
+  $katello_rhsm_setup_script      = "katello-rhsm-atomic-consumer"
+  $katello_rhsm_atomic_tar     = "katello-rhsm-atomic-consumer.tar"
+
   $candlepin_consumer_name        = "katello-ca-consumer-${::fqdn}"
   $candlepin_consumer_summary     = "Subscription-manager consumer certificate for Katello instance ${::fqdn}"
   $candlepin_consumer_description = 'Consumer certificate and post installation script that configures rhsm.'
+
+  $katello_atomic_base_dir_name   = "atomic"
+  $katello_atomic_dir             = "${katello_www_pub_dir}/${katello_atomic_base_dir_name}"
+  $sub_manager_cert_file_name     = "${certs::server_ca_name}.pem"
 
   include ::trusted_ca
   trusted_ca::ca { 'katello_server-host-cert':
@@ -52,5 +60,35 @@ class certs::katello (
     bootstrap_script => template('certs/rhsm-katello-reconfigure.erb'),
     alias            => $candlepin_cert_rpm_alias,
     subscribe        => $::certs::server_ca,
+  } ~>
+  # Setup the atomic pub dir
+  file { $katello_atomic_dir:
+    ensure => directory,
+    owner  => 'apache',
+    group  => 'apache',
+    mode   => '0755',
+    require => File["${katello_www_pub_dir}"]
+  } ->
+  # Placing the atomic rhsm setup script in the pub dir for atomic rhsm setup
+  file { "${katello_atomic_dir}/${katello_rhsm_setup_script}":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template('certs/rhsm-katello-atomic-configure.erb')
+  } ~>
+  # Placing the CA in the atomic pub dir to be included along with the tar gz
+  file { "${katello_atomic_dir}/${sub_manager_cert_file_name}":
+    ensure  => file,
+    source  => "${katello_www_pub_dir}/${certs::server_ca_name}.crt",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File["${katello_www_pub_dir}/${certs::server_ca_name}.crt"],
+  } ~>
+  exec { "setup_${katello_rhsm_atomic_tar}":
+  cwd     => "${katello_www_pub_dir}",
+  command => "/usr/bin/tar cf ${katello_rhsm_atomic_tar} ${katello_atomic_base_dir_name}",
+  require => File["${katello_atomic_dir}"],
   }
 }
