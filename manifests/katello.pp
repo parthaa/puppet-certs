990e26a2b6a1359e19ed3bc13838e531f5f26a02
Partha Aji <paji@redhat.com>
2015-09-09 16:32:21 -0400

    Fixes #10052 - Enabling consumer reg for atomic hosts
    Main intent of this commit is to setup rhsm conf on an atomic host to
    setup consumer registration (a role similar to the one played by the
    consumer bootstrap rpm).
    
    Artifacts produced
    
    1) /var/www/html/pub/katello-rhsm-consumer on the satellite. This script
    contains the code needed to setup rhsm conf on both atomic as well as
    non atomic machines. The contents of this script came out of the template
    used for post install script in the bootstrap consumer rpm. It has been
    tuned to work for both atomic/non-atomic machines
    
    2) Update to the bootstrap rpm to copy the new script to /usr/bin and
    run that as a part of post install.

--- manifests/katello.pp
+++ manifests/katello.pp
@@ -4,6 +4,7 @@ class certs::katello (
   $deployment_url                = undef,
   $rhsm_port                     = 443,
   $server_ca_name                = $::certs::server_ca_name,
+  $default_ca_name               = $::certs::default_ca_name,
   $candlepin_cert_rpm_alias_filename = undef
   ){
 
@@ -14,9 +15,15 @@ class certs::katello (
 
   $katello_www_pub_dir            = '/var/www/html/pub'
   $rhsm_ca_dir                    = '/etc/rhsm/ca'
+  $katello_rhsm_setup_script      = 'katello-rhsm-consumer'
+  $katello_rhsm_setup_script_location = "/usr/bin/${katello_rhsm_setup_script}"
+
   $candlepin_consumer_name        = "katello-ca-consumer-${::fqdn}"
   $candlepin_consumer_summary     = "Subscription-manager consumer certificate for Katello instance ${::fqdn}"
   $candlepin_consumer_description = 'Consumer certificate and post installation script that configures rhsm.'
+
+  $katello_default_ca_data = file("${certs::pki_dir}/certs/${certs::katello::default_ca_name}.crt")
+  $katello_server_ca_data = file("${certs::pki_dir}/certs/${certs::katello::server_ca_name}.crt")
 
   include ::trusted_ca
   trusted_ca::ca { 'katello_server-host-cert':
@@ -31,25 +38,30 @@ class certs::katello (
     mode   => '0755',
   } ->
   # Placing the CA in the pub dir for trusting by a user in their browser
-  file { "${katello_www_pub_dir}/${certs::server_ca_name}.crt":
+  file { "${katello_www_pub_dir}/${certs::katello::server_ca_name}.crt":
     ensure  => file,
-    source  => "${certs::pki_dir}/certs/${certs::server_ca_name}.crt",
+    source  => "${certs::pki_dir}/certs/${certs::katello::server_ca_name}.crt",
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    require => File["${certs::pki_dir}/certs/${certs::server_ca_name}.crt"],
+    require => File["${certs::pki_dir}/certs/${certs::katello::server_ca_name}.crt"],
   } ~>
-  # We need to deliver the server_ca for yum and rhsm to trust the server
-  # and the default_ca for goferd to trust the qpid
+  # Generate the the rhsm setup script in the pub dir for rhsm setup
+  file { "${katello_www_pub_dir}/${katello_rhsm_setup_script}":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    content => template('certs/rhsm-katello-reconfigure.erb'),
+  } ~>
   certs_bootstrap_rpm { $candlepin_consumer_name:
     dir              => $katello_www_pub_dir,
     summary          => $candlepin_consumer_summary,
     description      => $candlepin_consumer_description,
     # katello-default-ca is needed for the katello-agent to work properly
     # (especially in the custom certs scenario)
-    files            => ["${rhsm_ca_dir}/katello-default-ca.pem:644=${certs::pki_dir}/certs/${certs::default_ca_name}.crt",
-                        "${rhsm_ca_dir}/katello-server-ca.pem:644=${certs::pki_dir}/certs/${certs::server_ca_name}.crt"],
-    bootstrap_script => template('certs/rhsm-katello-reconfigure.erb'),
+    files            => ["${katello_rhsm_setup_script_location}:755=${katello_www_pub_dir}/${katello_rhsm_setup_script}"],
+    bootstrap_script => inline_template('/bin/bash <%= @katello_rhsm_setup_script_location %>'),
     alias            => $candlepin_cert_rpm_alias,
     subscribe        => $::certs::server_ca,
   }
--- spec/classes/certs_katello_spec.rb
+++ spec/classes/certs_katello_spec.rb
@@ -16,12 +16,19 @@ describe 'certs::katello' do
 
   context 'with parameters' do
     let :pre_condition do
-      "class {'certs': pki_dir => '/tmp', server_ca_name => 'foo'}"
+      "class {'certs': pki_dir => '/tmp', server_ca_name => 'server_ca', default_ca_name => 'default_ca'}"
     end
 
     describe 'with katello certs set' do
+      let!(:file) do
+        MockFunction.new('file') do  |f|
+          # The function will return 'blah blah' when 'blah' is passed in
+          f.expects(:call).with(['/tmp/certs/default_ca.crt']).returns('default ca cert')
+          f.expects(:call).with(['/tmp/certs/server_ca.crt']).returns('server ca cert')
+        end
+      end
       # source format should be -> "${certs::pki_dir}/certs/${server_ca_name}.crt"
-      it { should contain_trusted_ca__ca('katello_server-host-cert').with({ :source => "/tmp/certs/foo.crt" }) }
+      it { should contain_trusted_ca__ca('katello_server-host-cert').with({ :source => "/tmp/certs/server_ca.crt" }) }
     end
   end
 end
